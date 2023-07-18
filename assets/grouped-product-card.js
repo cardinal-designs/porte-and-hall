a04b0659fae383971d839d2ed38bc9e0b26c19a1
jackievalori <63308026+jackievalori@users.noreply.github.com>
2023-07-18 11:01:02 -0400

    discount flag math

--- assets/grouped-product-card.js
+++ assets/grouped-product-card.js
@@ -6,6 +6,7 @@ customElements.define('grouped-product-card', class GroupedProductCard extends H
     this.showSwatches = this.querySelector("[data-hidden-swatches]")
     this.swiper = this.querySelector(".swiper")
     this.atc = this.querySelector("[data-quick-add]")
+    this.cartDrawer = document.querySelector('cart-drawer');
 
     if(this.swiper){
       this.initSwiper(this.swiper)
@@ -24,17 +25,62 @@ customElements.define('grouped-product-card', class GroupedProductCard extends H
     }.bind(this))
   }
 
+  getSectionsToRender() {
+    return [
+      {
+        id: 'cart-drawer__content',
+        section: document.getElementById('cart-drawer__content').dataset.id,
+        selector: '.cart-drawer__content',
+      },
+      {
+        id: 'cart-icon-bubble',
+        section: 'cart-icon-bubble',
+        selector: '.shopify-section'
+      }
+    ];
+  }
+
+  getSectionInnerHTML(html, selector) {
+    return new DOMParser()
+      .parseFromString(html, 'text/html')
+      .querySelector(selector).innerHTML;
+  }
+
   quickAdd(t){
     t.classList.add('loading')
 
-    let formData = {
+    const body = JSON.stringify({
       'items': [{
        'id': t.dataset.variant,
        'quantity': 1
-       }]
-     };
+       }],
+      sections: this.getSectionsToRender().map((section) => section.section),
+      sections_url: window.location.pathname
+     });
+
+     console.log({ ...fetchConfig('javascript'), body })
 
     fetch(`${routes.cart_add_url}`, { ...fetchConfig('javascript'), body })
+    .then(response => response.json())
+    .then((parsedState) => {
+
+      console.log(parsedState)
+      this.getSectionsToRender().forEach((section => {
+        const elementToReplace =
+          document.getElementById(section.id).querySelector(section.selector) || document.getElementById(section.id);
+
+        elementToReplace.innerHTML =
+          this.getSectionInnerHTML(parsedState.sections[section.section], section.selector);
+
+      }));
+    })
+    .catch((e) => {
+      console.error(e);
+    })
+    .finally(() => {
+      t.classList.remove('loading')
+      this.cartDrawer.open();
+    });
   }
 
   showAllSwatches() {
