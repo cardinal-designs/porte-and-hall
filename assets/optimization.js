5944a53e2a93291593f080e47b5a28771eabe076
bestfriend-support <support@bestfriend.co>
2026-01-05 12:11:49 +0200

    Enhance lazy loading of scripts and images; add third-party script handling and improve swatch image rendering. Update CSS to hide consent SDK when scripts are not loaded. Refactor video section for better mobile handling.

--- assets/optimization.js
+++ assets/optimization.js
@@ -1,40 +1,59 @@
 (() => {
-    const userActivityEvents = [
-      "mousedown",
-      "mousemove",
-      "keydown",
-      "scroll",
-      "touchstart",
-      "keypress",
-      "touchmove",
-    ];
-    async function onUserActivity() {
-      const scriptTagsForLoad = [...document.querySelectorAll("script[data-lazy-src]")];
-      const linkTagsForLoad = document.querySelectorAll("link[rel=stylesheet][data-lazy-href]");
-      const scriptsLoad = [];
-      for (const scriptTag of scriptTagsForLoad) {
-        scriptTag.src = scriptTag.dataset.lazySrc;
-        const scriptLoad =  new Promise((resolve) => {
-          scriptTag.onload = resolve;
-          scriptTag.onerror = resolve;
-        })
-        scriptsLoad.push(scriptLoad);
-      }
-      for (const linkTag of linkTagsForLoad) {
-        linkTag.href = linkTag.dataset.lazyHref;
-      }
-      for (const event of userActivityEvents) {
-        window.removeEventListener(event, onUserActivity);
-      }
-      
-      window.dispatchEvent(new CustomEvent("load-head-scripts"));
-      await Promise.all(scriptsLoad);
-      window.dispatchEvent(new CustomEvent("lazy-scripts-loaded"));
+  const userActivityEvents = [
+    'mousedown',
+    'mousemove',
+    'keydown',
+    'scroll',
+    'touchstart',
+    'keypress',
+    'touchmove',
+  ];
+  const SCRIPTS_LOADED_FLAG = 'third-party-scripts-loaded';
+
+  async function onUserActivity() {
+    for (const event of userActivityEvents) {
+      window.removeEventListener(event, onUserActivity);
     }
-    function startListenUserActivity() {
-      for (const event of userActivityEvents) {
-        window.addEventListener(event, onUserActivity);
-      }
+
+    const scriptTagsForLoad = Array.from(
+      document.querySelectorAll('script[data-lazy-src]')
+    );
+
+    const scriptsLoad = [];
+
+    for (const scriptTag of scriptTagsForLoad) {
+      scriptTag.src = scriptTag.dataset.lazySrc;
+
+      const scriptLoad = new Promise((resolve) => {
+        scriptTag.onload = resolve;
+        scriptTag.onerror = resolve;
+      });
+
+      scriptsLoad.push(scriptLoad);
     }
-    window.addEventListener("load", startListenUserActivity);
-  })();
+
+    window.dispatchEvent(new CustomEvent('load-head-scripts'));
+
+    await Promise.all(scriptsLoad);
+
+    localStorage.setItem(SCRIPTS_LOADED_FLAG, 'true');
+
+    window.dispatchEvent(new CustomEvent('third-party-scripts-loaded'));
+
+    document.documentElement.classList.add('third-party-scripts-loaded');
+  }
+
+  function startListenUserActivity() {
+    for (const event of userActivityEvents) {
+      window.addEventListener(event, onUserActivity);
+    }
+  }
+
+  window.addEventListener('load', () => {
+    if (localStorage.getItem(SCRIPTS_LOADED_FLAG)) {
+      onUserActivity();
+    } else {
+      startListenUserActivity();
+    }
+  });
+})();
